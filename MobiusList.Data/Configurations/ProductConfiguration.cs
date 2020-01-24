efff72adb31124053c305270c70a51436f7bced6
Rick Hallett <rickhallett@live.co.uk>
2020-01-24 22:16:53 +0000

    ProductConfiguration, MobiusDbContext init

---
+++ MobiusList.Data/Configurations/ProductConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MobiusList.Core.Models;
+
+namespace MobiusList.Data.Configurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder
+                .HasKey(p => p.ProductId);
+            
+            builder
+                .Property(p => p.ProductId)
+                .UseIdentityColumn();
+            
+            builder
+                .Property(p => p.ProductNumber)
+                .IsRequired();
+            
+            builder
+                .Property(p => p.Name)
+                .IsRequired();
+
+            builder
+                .HasOne(p => p.Category);
+
+            builder
+                .ToTable("Products");
+        }
+    }
+}
---
+++ MobiusList.Data/MobiusDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MobiusList.Core.Models;
+using MobiusList.Data.Configurations;
+
+namespace MobiusList.Data
+{
+    public class MobiusDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public MobiusDbContext(DbContextOptions<MobiusDbContext> options) 
+            : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+        }
+    }
+}
