e26acec1cd3a6b8dca5d4ee4556d878406b8deb2
Rick Hallett <rickhallett@live.co.uk>
2020-01-24 22:00:39 +0000

    define product service interface

---
+++ MobiusList.Core/Services/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MobiusList.Core.Models;
+
+namespace MobiusList.Core.Services
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<Product>> GetAllProducts();
+        Task<Product> GetProductById(Guid id);
+        Task<IEnumerable<Product>> GetProductByCategory(string name);
+        Task<Product> CreateProduct(Product newProduct);
+        Task UpdateProduct(Product productToBeUpdated, Product product);
+        Task DeleteProduct(Product product);
+    }
+}
