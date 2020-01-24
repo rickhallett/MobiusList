dcf07004688604d8a6231791f63af16324a3ca82
Rick Hallett <rickhallett@live.co.uk>
2020-01-24 21:56:46 +0000

    define repository interfaces

---
+++ MobiusList.Core/Repositories/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MobiusList.Core.Models;
+
+namespace MobiusList.Core.Repositories
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+        Task<IEnumerable<Product>> GetAllProductsAsync();
+        Task<Product> GetProductByIdAsync(Guid id);
+        Task<IEnumerable<Product>> GetProductsByCategoryName(string name);
+    }
+}
---
+++ MobiusList.Core/Repositories/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MobiusList.Core.Repositories
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IProductRepository Products { get; }
+        Task<int> CommitAsync();
+    }
+}
